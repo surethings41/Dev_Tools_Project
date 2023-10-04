f3138b078ab78c0e7fa4e9a007744717d8f485fa
surethings41 <140444126+surethings41@users.noreply.github.com>
2023-10-04 13:00:52 -0500

    app.py histogram test

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd 
+import plotly.express as px
+
+column_names= ["price", "model_year", "model", "condition", "cylinders", "fuel", "odometer", "transmission", "type", "paint_color", "is_4wd", "date_posted", "days_listed"]
+
+
+df = pd.read_csv('/Users/jeffreyheller/Desktop/Dev_Tools_Project/vehicles_us.csv', sep =',', header=None, names=column_names)
+
+df = df.iloc[2:]
+
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+
+st.header('US Vehicles Data Sheet') 
+st.dataframe(df)
+
+
+st.header('Mileage Based on Vehicle Type')
+# histogram figure
+fig = px.histogram(df, x='odometer', y='type', color='manufacturer')
+
+st.write(fig)
