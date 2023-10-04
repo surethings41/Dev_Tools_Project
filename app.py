071fda068c587e8bd98b075d3f4f50ddb6eaa7e0
surethings41 <140444126+surethings41@users.noreply.github.com>
2023-10-04 15:14:51 -0500

    5th test

--- app.py
+++ app.py
@@ -17,25 +17,15 @@ st.dataframe(df)
 
 st.header('Mileage Based on Vehicle Type')
 # histogram figure
-fig = px.bar(df, x='type', y='odometer', color='manufacturer', barmode='group',
-             labels={'odometer': 'Average Odometer', 'type': 'Vehicle Type'},
-             title='Average Odometer by Vehicle Type and Manufacturer')
-
-st.plotly_chart(fig)
-
-st.header('Car Manufacture Market Share ')
-
-manufacturer_counts = df['manufacturer'].value_counts().reset_index()
-
-manufacturer_counts.columns = ['manufacturer', 'count']
+manufacturer_avg_odometer = df.groupby('manufacturer')['odometer'].mean().reset_index()
 
 # Create a Plotly Express bar chart
-fig = px.bar(manufacturer_counts, x='manufacturer', y='count', title='Car Manufacturer Market Share')
+fig = px.bar(manufacturer_avg_odometer, x='manufacturer', y='odometer', title='Average Odometer Reading by Manufacturer')
 
 # Customize the chart layout if needed
-fig.update_layout(xaxis_title='Manufacturer', yaxis_title='Count')
+fig.update_layout(xaxis_title='Manufacturer', yaxis_title='Average Odometer (Miles)')
 fig.update_traces(marker_color='blue')  # You can change the marker_color as desired
 
 # Display the chart using st.plotly_chart
-st.title('Car Manufacturer Market Share')
+st.title('Average Odometer Reading by Manufacturer')
 st.plotly_chart(fig)
